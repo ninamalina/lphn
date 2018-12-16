b1176fcea9d883517a901f845124773369137701
ninamalina <nina.mrzelj@gmail.com>
2018-12-16 20:54:47 +0100

    gaehet optimier bug

--- gaehet/optimizer.py
+++ gaehet/optimizer.py
@@ -26,7 +26,7 @@ class OptimizerAE(object):
 
         labels = tf.sparse_tensor_to_dense(labels, validate_indices=False)
         # loss = norm * tf.nn.weighted_cross_entropy_with_logits(targets=labels,  logits=preds, pos_weight=pos_weight)
-        loss =  tf.nn.sigmoid_cross_entropy_with_logits(targets=labels,  logits=preds)
+        loss =  tf.nn.sigmoid_cross_entropy_with_logits(labels=labels,  logits=preds)
         # loss =  tf.nn.softmax_cross_entropy_with_logits(targets=labels,  logits=preds)
 
         self.cost = tf.reduce_mean(loss)
