56967cda1500801e409b0f2ab170b888ba0008f8
ninamalina <nina.mrzelj@gmail.com>
2018-12-17 20:12:37 +0100

    simple model - feature selection

--- simple_model.py
+++ simple_model.py
@@ -12,10 +12,10 @@ from utils import read_split
 
 class SimpleClassifier:
 
-    def __init__(self, G_train, train_edges, test_positive, test_negative, path, feat_id):
+    def __init__(self, G_train, train_edges, test_positive, test_negative, val_positive, val_negative, path):
         self.train_edges = train_edges
         self.test_edges = np.concatenate((test_positive, test_negative), axis=0)
-        self.feat_id = feat_id
+        self.val_edges = np.concatenate((val_positive, val_negative), axis=0)
 
         if os.path.exists(path + "train_edges.npy") :
             print ("Reading files")
@@ -25,6 +25,8 @@ class SimpleClassifier:
                 self.Y_train = np.load(path + "Y_train.npy")
                 self.X_test = np.load(path + "X_test.npy")
                 self.Y_test = np.load(path + "Y_test.npy")
+                self.X_val = np.load(path + "X_val.npy")
+                self.Y_val = np.load(path + "Y_val.npy")
             else:
                 print("Something is wrong ... ")
         else:
@@ -32,22 +34,39 @@ class SimpleClassifier:
             self.prepare_train(G_train, train_edges)
             Y_test = [1 for i in test_positive] + [0 for i in test_negative]
             self.prepare_test(G_train, self.test_edges, Y_test)
+            Y_val = [1 for i in val_positive] + [0 for i in val_negative]
+            self.prepare_val(G_train, self.val_edges, Y_val)
             self.save(path)
 
 
     def train(self, method, seed=0):
-        if method == "RF":
-            self.clf = RandomForestClassifier(n_estimators=10)
-        elif method == "LASSO":
-            self.clf = linear_model.Lasso(alpha = 0.1)
-        elif method == "LR":
+        if method == "LR":
             self.clf = linear_model.LogisticRegression(class_weight="balanced", random_state=seed)
-        elif method == "AB":
-            self.clf = AdaBoostClassifier(n_estimators=100)
-        elif method == "SGD":
-            self.clf = linear_model.SGDClassifier(max_iter=1000)
+            self.clf.fit(self.X_train, self.Y_train)
+        elif method == "FS": # feature selection
+            features = {"degrees" : [0, 1], "prod": [2], "neighbors":[3], "jaccard":[4], "adamic":[5], "pagerank":[6,7]}
+            best_features = []
+            best_roc = 0
+            is_improving = True
+            while is_improving:
+                is_improving = False
+                for feature in features:
+                    current_selected = best_features + features[feature]
+                    print(current_selected)
+                    clf = linear_model.LogisticRegression(class_weight="balanced", random_state=seed)
+                    clf.fit(self.X_train[:,current_selected], self.Y_train)
+                    pred = clf.predict(self.X_val)
+                    current_roc = roc_auc_score(self.Y_val, pred)
+                    if current_roc > best_roc:
+                        is_improving = True
+                        feature_to_add = feature
+                if is_improving:
+                    best_features = best_features + features[feature_to_add]
+                    features = {feature:features[feature] for feature in features if feature!=feature_to_add}
 
-        self.clf.fit(self.X_train, self.Y_train)
+            self.clf = linear_model.LogisticRegression(class_weight="balanced", random_state=seed)
+            self.clf.fit(self.X_train[:,best_features], self.Y_train)
+            print(best_features)
 
     def predict(self, prob=False):
         if prob:
@@ -79,23 +98,15 @@ class SimpleClassifier:
             page_rank_0 = page_rank[pair[0]]
             page_rank_1 = page_rank[pair[1]]
 
-            if self.feat_id == 0:
-                f = [degree_0,
-                     degree_1,
-                     prod,
-                     commmon_neighbors,
-                     jaccard_coefficient,
-                     adamic_adar,
-                     page_rank_0,
-                     page_rank_1,
-                     ]
-            elif self.feat_id == 1:
-                f = [degree_0,
-                     degree_1,
-                     prod,
-                     page_rank_0,
-                     page_rank_1,
-                     ]
+            f = [degree_0,
+                 degree_1,
+                 prod,
+                 commmon_neighbors,
+                 jaccard_coefficient,
+                 adamic_adar,
+                 page_rank_0,
+                 page_rank_1,
+                 ]
 
             X.append(f)
 
@@ -113,11 +124,17 @@ class SimpleClassifier:
         self.X_test = self.create_features(G_train, test_edges)
         self.Y_test = Y_test
 
+    def prepare_val(self, G_train, val_edges, Y_val):
+        self.X_val = self.create_features(G_train, val_edges)
+        self.Y_val = Y_val
+
     def save(self, path):
         np.save(path + "X_train", self.X_train)
         np.save(path + "X_test", self.X_test)
+        np.save(path + "X_val", self.X_val)
         np.save(path + "Y_train", self.Y_train)
         np.save(path + "Y_test", self.Y_test)
+        np.save(path + "Y_val", self.Y_val)
         np.save(path + "train_edges", self.train_edges)
 
 
@@ -128,7 +145,7 @@ if __name__ == '__main__':
     graph_path = sys.argv[2] # "data/bio/parsed/bio_edgelist.tsv"
     edge_type = sys.argv[3].split("_") # disease_gene
     num = int(sys.argv[4]) # 0
-    feat_id = int(sys.argv[5])
+    method = sys.argv[5] # LR or FS
 
     f = open(graph_path)
     G = nx.Graph()
@@ -143,9 +160,9 @@ if __name__ == '__main__':
     p = path + "random_splits/" + edge_type[0] + "_" + edge_type[1] + "/random" + str(num) + "/"
     G_train, test_positive, test_negative, val_positive, val_negative, train_edges = read_split(GC, edge_type, num, p)
 
-    p = path + "preprocessed_simple/" + edge_type[0] + "_" + edge_type[1] + "/random" + str(num) + "/" + str(feat_id) + "/"
-    simple_model = SimpleClassifier(G_train, train_edges, test_positive, test_negative, p, feat_id)
-    simple_model.train("LR", num)
+    p = path + "features/" + edge_type[0] + "_" + edge_type[1] + "/random" + str(num) + "/"
+    simple_model = SimpleClassifier(G_train, train_edges, test_positive, test_negative, val_positive, val_negative, p)
+    simple_model.train(method, num)
     simple_model.predict()
     print("Acc:", simple_model.evaluate())
     simple_model.predict(prob=True)
