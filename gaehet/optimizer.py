1f7461e0ff3841e76848b0bab29669c3999e0dc5
ninamalina <nina.mrzelj@gmail.com>
2018-12-18 01:10:53 +0100

    het logging

--- gaehet/optimizer.py
+++ gaehet/optimizer.py
@@ -26,10 +26,13 @@ class OptimizerAE(object):
 
         labels = tf.sparse_tensor_to_dense(labels, validate_indices=False)
         # loss = norm * tf.nn.weighted_cross_entropy_with_logits(targets=labels,  logits=preds, pos_weight=pos_weight)
+        print(labels)
+        print(preds)
         loss =  tf.nn.sigmoid_cross_entropy_with_logits(labels=labels,  logits=preds)
         # loss =  tf.nn.softmax_cross_entropy_with_logits(targets=labels,  logits=preds)
 
         self.cost = tf.reduce_mean(loss)
+        print(self.cost)
 
         # self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.labels, logits=self.predictions))
         # self.cost = norm * tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=preds_sub, targets=labels_sub, pos_weight=pos_weight))
