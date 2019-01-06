76f47b40f322400ecc99316460890121bfe0bfe6
ninamalina <nina.mrzelj@gmail.com>
2019-01-07 00:30:14 +0100

    amazon metapaths

--- meta_model.py
+++ meta_model.py
@@ -7,7 +7,7 @@ from sklearn import linear_model
 import time
 import sys
 from utils import read_split
-from generate_meta_paths import MetaPathGeneratorBio, MetaPathGeneratorSicris, MetaPathGeneratorImdb
+from generate_meta_paths import MetaPathGeneratorBio, MetaPathGeneratorSicris, MetaPathGeneratorImdb, MetaPathGeneratorAmazon
 from subprocess import call
 from collections import defaultdict
 
@@ -146,6 +146,8 @@ if __name__ == '__main__':
         mpg = MetaPathGeneratorSicris(random_seed)
     elif dataset == "imdb":
         mpg = MetaPathGeneratorImdb(random_seed)
+    elif dataset == "amazon":
+        mpg = MetaPathGeneratorAmazon(random_seed)
 
     mpg.read_data(G_train)
 
